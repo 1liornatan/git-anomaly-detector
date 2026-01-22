79b7347bad4c729429b8e44e173d288e9beccfd9
1liornatan <1liornatan@gmail.com>
2026-01-22 23:24:18 +0200

    Refactor UnixTimeStampToDateTime method to use DateTimeOffset for improved accuracy

--- GitAnomalyDetector/Utils/ObjectMapper.cs
+++ GitAnomalyDetector/Utils/ObjectMapper.cs
@@ -56,8 +56,7 @@ namespace GitAnomalyDetector.Utils
 
         private static DateTime UnixTimeStampToDateTime(long unixTimeStamp)
         {
-            var dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-            dateTime = dateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            var dateTime = DateTimeOffset.FromUnixTimeSeconds(unixTimeStamp).DateTime;
             return dateTime;
         }
 
